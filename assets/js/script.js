4978f2f4a642f7d71203adf23d5f23aecc561d4e
Stacey robson <stacey.robson.sr@gmail.com>
2023-10-12 09:28:48 +0000

    data types added to html and buttons show a box with correct javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,37 @@
+// wait for dom to finish loading
+
+// listens for clicks 
+// document.addEventListener("DOMContentLoaded", function() {
+//     let buttons = document.getElementByTagName("button");
+//     for (let button of buttons) {
+//         button.addEventListener("click", function(){
+//             if (this.getAttribute("data-type") === "submit")
+//                 alert("You Clicked Submit!"); {
+//             } else {
+//                 let gamType = this.getAttribute("data-type");
+//                 alert(`You clicked $(gameType)`);
+//             }
+//         })
+
+//     }
+
+// });
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+
 function runGame() {
 
 }
